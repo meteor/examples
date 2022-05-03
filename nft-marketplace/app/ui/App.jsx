6a9a54e55d8e569a486f5f121f1758cdb5f0c509
Vitor Flores <sf.joaovitor@gmail.com>
2022-05-03 17:33:13 -0400

    Added TailwindCSS

--- nft-marketplace/app/ui/App.jsx
+++ nft-marketplace/app/ui/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1 className="text-3xl text-indigo-800">Welcome to Meteor!</h1>
   </div>
 );
---
+++ nft-marketplace/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./app/ui/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
