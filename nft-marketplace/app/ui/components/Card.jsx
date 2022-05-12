7574e3085f30f7c1b1200738f22d7833b0f9f1eb
Vitor Flores <sf.joaovitor@gmail.com>
2022-05-12 10:46:10 -0400

    Fixed badge background color on Card component

--- nft-marketplace/app/ui/components/Card.jsx
+++ nft-marketplace/app/ui/components/Card.jsx
@@ -12,13 +12,13 @@ export const Card = ({
   badge = '',
 }) => {
   const navigate = useNavigate();
-  const badgeBackgroundColor = (badge === "owned") ? "rhino" : "dodger";
+  const badgeBackgroundColor = (badge === "owned") ? "bg-burst" : "bg-dodger";
 
   return (
     <div className={`cursor-pointer ${className}`} onClick={() => navigate(`${RoutePaths.DETAILS}/${itemId}`)}>
       <div className="relative rounded-t-lg max-h-80 flex items-center overflow-hidden">
         {badge && (
-          <p className={`absolute top-0 left-0 text-small text-white font-medium uppercase rounded-br-lg py-1 px-2 bg-${badgeBackgroundColor}`}>{badge}</p>
+          <p className={`absolute top-0 left-0 text-small text-white font-medium uppercase rounded-br-lg py-1 px-2 ${badgeBackgroundColor}`}>{badge}</p>
         )}
         <img className="w-full" src={itemImg} />
       </div>
