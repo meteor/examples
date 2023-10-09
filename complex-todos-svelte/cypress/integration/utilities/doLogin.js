057306b7402becaa42889cd8a64a859d66dc61f5
gunce <guncebektas@gmail.com>
2023-10-09 13:48:58 +0300

    fix: test

--- complex-todos-svelte/cypress/integration/utilities/doLogin.js
+++ complex-todos-svelte/cypress/integration/utilities/doLogin.js
@@ -1,5 +1,5 @@
-import {TEST} from '../constants/constantsForTests.js';
 import {waitForMeteorSubscriptions} from '../../support/commands.js';
+import {TEST} from "../constants/test.js";
 
 /**
  * Login with provided user name and password (subscription aware)
@@ -9,21 +9,21 @@ import {waitForMeteorSubscriptions} from '../../support/commands.js';
 export function doLogin(username = TEST.USER.DEMO.USERNAME, password = TEST.USER.DEMO.PASSWORD)
 {
   cy.visit('/');
-  
+
   cy.get('#login-sign-in-link').click();
-  
+
   cy.get('#login-username').clear().type(TEST.USER.TEST.USERNAME);
-  
+
   cy.get('#login-password').clear().type(TEST.USER.TEST.PASSWORD);
-  
+
   cy.get('#login-buttons-password').click();
-  
+
   cy.get('#login-name-link').should('contain', TEST.USER.TEST.USERNAME);
-  
+
   cy.window().its('Meteor').invoke('userId').should((userId) =>
   {
     expect(userId).not.to.be.null;
   });
-  
+
   waitForMeteorSubscriptions();
 }
