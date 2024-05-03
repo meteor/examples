820248f136fd4cf2c2e2215a9269c0956941af1a
Leonardo Venturini <leovenbag@gmail.com>
2024-05-03 07:02:46 -0400

    bypass collection2

--- complex-todos-svelte/imports/modules/tasks/database/tasks.js
+++ complex-todos-svelte/imports/modules/tasks/database/tasks.js
@@ -1,33 +1,5 @@
-import {Mongo} from 'meteor/mongo';
-import SimpleSchema from 'simpl-schema';
+import { Mongo } from 'meteor/mongo';
 
 const Tasks = new Mongo.Collection('tasks');
-
-Tasks.attachSchema(new SimpleSchema({
-  text: {
-    type: String
-  },
-  createdAt: {
-    type: Date
-  },
-  owner: {
-    type: String
-  },
-  username: {
-    type: String
-  },
-  checked: {
-    type: Boolean,
-    optional: true
-  },
-  expired: {
-    type: Boolean,
-    optional: true
-  },
-  private: {
-    type: Boolean,
-    optional: true
-  }
-}));
 
 export {Tasks};
