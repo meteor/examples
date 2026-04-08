d35508da5019f6dfe826bfad38824b507fbd6e43
Nacho Codoñer <igcogi@gmail.com>
2026-04-08 17:29:07 +0200

    upgrade dependencies and switch to Tailwind CSS 4.2.2

--- task-manager/rspack.config.js
+++ task-manager/rspack.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig(Meteor => {
         Meteor.isClient && {
           test: /\.css$/,
           use: ['postcss-loader'],
-          type: 'css/auto',
+          type: 'css',
         },
       ].filter(Boolean),
     },
